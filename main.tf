1e3e7b684ae162e823abe38b1decefb683592481
Adam Levi <adamlevi87@gmail.com>
2025-02-09 20:46:14 +0200

    change escape character not needed

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "null_resource" "dns_records_failure" {
   provisioner "local-exec" {
     interpreter = ["/bin/bash" ,"-c"]
     command = <<-EOT
-      echo -e '\\e[31mTerraform is going to fail, you must run Terraform Output and create the required DNS records and then retry\\e[0m'
+      echo -e '\e[31mTerraform is going to fail, you must run Terraform Output and create the required DNS records and then retry\e[0m'
       echo "use this link: https://${var.region}.console.aws.amazon.com/acm/home?region=${var.region}#/certificates/${aws_acm_certificate.cert.arn}" | sed 's|arn:.*certificate/||'
     EOT
   }
